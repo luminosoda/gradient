7de03787c9b822ab856cb0cad75813a46f0a718b
luminosoda <luminosoda@youngwaves.ru>
2020-05-23 05:44:27 +0500

    :sparkles: Linear gradient

--- gradient.nim
+++ gradient.nim
@@ -1,3 +1,17 @@
+import chroma
+export chroma
+
+
 proc interpolate(first, second, step, total: float32): float32 =
   ## Return result of interpolation 2 color values at `step`/`total
   result = first + (step / total) * (second - first)
+
+proc linear_gradient*(first, second: Color, count: int): seq[Color] =
+  ## Simple linear gradient from 2 colors, return sequence of colors with `count` length
+  for step in 0..count - 1:
+    result.add(Color(
+      r: interpolate(first.r, second.r, float32(step), float32(count - 1)),
+      g: interpolate(first.g, second.g, float32(step), float32(count - 1)),
+      b: interpolate(first.b, second.b, float32(step), float32(count - 1)),
+      a: interpolate(first.a, second.a, float32(step), float32(count - 1))
+    ))
