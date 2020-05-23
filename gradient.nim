099ba0dc0911904b2eda0c6028b4afc9fbdcdfbc
luminosoda <luminosoda@youngwaves.ru>
2020-05-23 06:22:34 +0500

    :sparkles: Polylinear gradient

--- gradient.nim
+++ gradient.nim
@@ -15,3 +15,10 @@ proc linear_gradient*(first, second: Color, count: int): seq[Color] =
       b: interpolate(first.b, second.b, float32(step), float32(count - 1)),
       a: interpolate(first.a, second.a, float32(step), float32(count - 1))
     ))
+
+proc polylinear_gradient*(colors: openArray[Color], count: int): seq[Color] =
+  ## Polylinear gradient from openArray of colors, return sequence of colors with `count` length
+  let colors_per_gradient = int(count / (len(colors) - 1))
+
+  for i in 0..colors.len - 2:
+    result.add(linear_gradient(colors[i], colors[i + 1], colors_per_gradient))
